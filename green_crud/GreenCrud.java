0cdde5a1d1aa1e36dcee56b32b213212e42e452e
eps <eps46656@gmail.com>
2021-04-01 15:50:45 +0800

    2021-04-01-15-50

--- green_crud/GreenCrud.java
+++ green_crud/GreenCrud.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class GreenCrud {
 	/**
 	* The minimum acceptable initialSize of calPopulation.
@@ -36,7 +38,7 @@ public class GreenCrud {
 	* @param days The green crud population after days days.
 	* @return An integer, which is the size of the green crud population after days days, starting at an initial size of initialSize.
 	*/
-	public tatic int calPopulation(int initialSize, int days) {
+	public static int calPopulation(int initialSize, int days) {
 		if (fibonacciInitialed_ == false) {
 			fibonacci_[0] = 1;
 			fibonacci_[1] = 1;
